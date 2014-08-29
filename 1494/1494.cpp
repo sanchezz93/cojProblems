f1ac82715f82a302feaff00fd612b08b8aee7fe8
Alejandro Sanchez <alejandro.s1993@gmail.com>
2014-08-28 22:25:32 -0500

    Accepted 1494

---
+++ 1494/1494.cpp
@@ -0,0 +1,18 @@
+//
+//  1494.cpp
+//  
+//
+//  Created by Alejandro Sanchez on 8/28/14.
+//
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    string input;
+    cin >> input;
+    cout << "I got my first solution!!!" << endl;
+    return 0;
+}
